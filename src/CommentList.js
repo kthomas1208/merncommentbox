196742e8558dbfb80fb2340486c5c6ee79663824
kevinthomas <kthomas1208@gmail.com>
2017-01-05 01:26:39 -0500

    the thing works, now

--- src/CommentList.js
+++ src/CommentList.js
@@ -1,5 +1,5 @@
 //CommentList.js
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Comment from './Comment';
 import style from './style';
 
@@ -7,14 +7,19 @@ class CommentList extends Component {
   render() {
     let commentNodes = this.props.data.map(comment => {
       return (
-        <Comment author={ comment.author} key={comment.id}>
-          {comment.text}
+        <Comment
+          author={ comment.author }
+          uniqueID={ comment['_id'] }
+          onCommentDelete={ this.props.onCommentDelete }
+          onCommentUpdate={ this.props.onCommentUpdate }
+          key={ comment['_id'] }>
+          { comment.text }
         </Comment>
       )
     })
     return (
-      <div style={style.commentList}>
-        {commentNodes}
+      <div style={ style.commentList }>
+        { commentNodes }
       </div>
     )
   }
